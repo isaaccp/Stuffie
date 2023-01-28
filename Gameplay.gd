8577e554c4e4ede8b936d4918aaf2191b1233a0f
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-01-28 16:28:39 -0500

    New character scene!

--- Gameplay.gd
+++ Gameplay.gd
@@ -5,13 +5,35 @@ enum GameState {
   CPU_TURN,
 }
 
-var state = GameState.HUMAN_TURN
+var state_text = {
+	GameState.HUMAN_TURN: "Your turn",
+	GameState.CPU_TURN: "Enemy turn",
+}
+
+var state
+var cpu_turn_start = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	change_state(GameState.HUMAN_TURN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if state == GameState.HUMAN_TURN:
+		# wait for human to finish turn
+		pass
+	else:
+		if cpu_turn_start == -1:
+			cpu_turn_start = Time.get_ticks_msec()
+		if Time.get_ticks_msec() - cpu_turn_start > 1000:
+			change_state(GameState.HUMAN_TURN)
+			cpu_turn_start = -1
+		
+
+func change_state(new_state):
+	state = new_state
+	$UI/TurnState.text = state_text[state]
+	
+func _on_end_turn_button_pressed():
+	change_state(GameState.CPU_TURN)
+	
---
+++ character.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var total_ap = 5
+var total_mp = 10
+var ap
+var mp
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
