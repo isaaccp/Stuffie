2e9a257aa88970b0f9578c399971b922d94faed7
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-01-28 15:40:10 -0500

    Very beginning

---
+++ Gameplay.gd
@@ -0,0 +1,17 @@
+extends Node
+
+enum GameState {
+  HUMAN_TURN,
+  CPU_TURN,
+}
+
+var state = GameState.HUMAN_TURN
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
