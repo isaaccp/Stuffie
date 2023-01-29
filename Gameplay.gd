54bda12841b4714793e51c2efd62d53b2a740dd8
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-01-28 23:01:02 -0500

    A* implementation (and some proof of concept)

--- Gameplay.gd
+++ Gameplay.gd
@@ -16,6 +16,8 @@ var active_character: Character
 
 var state
 var cpu_turn_start = -1
+var a_star
+var path
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,6 +33,30 @@ func _ready():
 		i += 1
 	set_active_character(0)
 	change_state(GameState.HUMAN_TURN)
+	build_a_star()
+	path = a_star.get_point_path(Vector2i(3, 3), Vector2i(32, 20))
+	print_debug(path)
+	for point in path:
+		$World/Line2D.add_point(point+Vector2(8,8))
+	
+func build_a_star():
+	a_star = AStarGrid2D.new()
+	var map_rect = $World/TileMap.get_used_rect()
+	a_star.size = map_rect.size
+	a_star.cell_size = Vector2(16, 16)
+	a_star.diagonal_mode = a_star.DIAGONAL_MODE_NEVER
+	a_star.set_default_estimate_heuristic(AStarGrid2D.HEURISTIC_CHEBYSHEV)
+	a_star.set_default_compute_heuristic(AStarGrid2D.HEURISTIC_CHEBYSHEV)
+	a_star.update()
+	for i in map_rect.size[0]:
+		for j in map_rect.size[1]:
+			var tile_data = $World/TileMap.get_cell_tile_data(0, Vector2i(i, j))
+			var solid = tile_data.get_custom_data("Solid") as bool
+			if solid:
+				a_star.set_point_solid(Vector2i(i, j))
+
+	for pos in $World/TileMap.get_used_cells(1):
+		a_star.set_point_solid(pos)
 
 func _on_character_portrait_pressed(index: int):
 	if state != GameState.HUMAN_TURN:
