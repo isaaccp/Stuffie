312724c0448b740671c983165c48d17c74304bfa
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-02-16 18:00:09 -0500

    Migrate blacksmith to new state machine.
    Part of #40.

--- blacksmith.gd
+++ blacksmith.gd
@@ -10,12 +10,23 @@ class_name BlacksmithStage
 # attach sub-screen under advanced_option_parent.
 @export var advanced_option_parent: Control
 
-enum State {
-	CHOOSING_OPTION,
-	REMOVAL,
-	UPGRADE,
-	DONE,
-}
+class BlacksmithState extends StateMachine.State:
+	pass
+	
+class ChoosingOption extends BlacksmithState:
+	var name = "ChoosingOption"
+	
+class Removal extends BlacksmithState:
+	var name = "Removal"
+	
+class Upgrade extends BlacksmithState:
+	var name = "Upgrade"
+
+class Done extends BlacksmithState:
+	var name = "Done"
+
+func class_check(state: BlacksmithState):
+	return (state is BlacksmithState)
 
 
 # TODO: Eventually we'll display a box of e.g. 4 cards for purchase,
@@ -25,7 +36,7 @@ enum State {
 var removal_cost = 0
 var available_removals = 1
 
-var state = State.CHOOSING_OPTION
+var state = StateMachine.new(class_check)
 var characters: Array[Character]
 var current_cards: Array[Card]
 var card_ui_scene = preload("res://card_ui.tscn")
@@ -34,8 +45,28 @@ var removal_scene = preload("res://card_removal.tscn")
 signal stage_done
 
 func _ready():
+	state.connect("state_entered", _on_state_entered)
+	state.connect("state_exited", _on_state_exited)
+	state.change_state(ChoosingOption.new())
 	advanced_option_parent.hide()
 
+func _on_state_entered(new_state: BlacksmithState):
+	if new_state is ChoosingOption:
+		main.show()
+	elif new_state is Removal:
+		var removal = removal_scene.instantiate() as CardRemoval
+		removal.initialize(characters)
+		removal.connect("done", _on_removal_done)
+		removal.connect("canceled", _on_removal_canceled)
+		advanced_option_parent.add_child(removal)
+		advanced_option_parent.show()
+	
+func _on_state_exited(state: BlacksmithState):
+	if state is ChoosingOption:
+		main.hide()
+	elif state is Removal:
+		advanced_option_parent.hide()
+	
 func initialize(characters: Array[Character]):
 	self.characters = characters
 	update_removals()
@@ -43,30 +74,10 @@ func initialize(characters: Array[Character]):
 func _process(delta):
 	pass
 
-func change_state(new_state: State):
-	if state == new_state:
-		return
-	# Hide main UI.
-	if state == State.CHOOSING_OPTION:
-		main.hide()
-	elif state == State.REMOVAL:
-		advanced_option_parent.hide()
-	# Show new UI.
-	if new_state == State.CHOOSING_OPTION:
-		main.show()
-	elif new_state == State.REMOVAL:
-		var removal = removal_scene.instantiate() as CardRemoval
-		removal.initialize(characters)
-		removal.connect("done", _on_removal_done)
-		removal.connect("canceled", _on_removal_canceled)
-		advanced_option_parent.add_child(removal)
-		advanced_option_parent.show()
-	state = new_state
-		
 func _on_removal_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.pressed == true:
-			change_state(State.REMOVAL)
+			state.change_state(Removal.new())
 
 func update_removals():
 	if available_removals == 0:
@@ -76,10 +87,10 @@ func update_removals():
 func _on_removal_done():
 	available_removals -= 1
 	update_removals()
-	change_state(State.CHOOSING_OPTION)
+	state.change_state(ChoosingOption.new())
 	
 func _on_removal_canceled():
-	change_state(State.CHOOSING_OPTION)
+	state.change_state(ChoosingOption.new())
 
 func _on_done_pressed():
 	stage_done.emit()
