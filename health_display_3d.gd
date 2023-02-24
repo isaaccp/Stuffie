1abdc909285e0286edde5d5b4848eb9098ab2324
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-02-24 18:47:21 -0500

    (Hopefully) fix annoying errors in health_display_3d.

--- health_display_3d.gd
+++ health_display_3d.gd
@@ -4,10 +4,10 @@ class_name HealthDisplay3D
 
 @export var sprite: Sprite3D
 @export var health_display: HealthDisplay
-@export var viewport: SubViewport
+@export var texture: ViewportTexture
 
 func _ready():
-	sprite.texture = viewport.get_texture()
+	sprite.texture = texture
 
 func update_health(value, full):
 	health_display.update_health(value, full)
