1685b7c7d344ba5032fa2a5ff42333b488d40907
baptr <1522777+baptr@users.noreply.github.com>
2023-02-25 02:49:40 -0800

    WIP simplify Highlight3D

--- highlight.gd
+++ highlight.gd
@@ -22,12 +22,12 @@ func _init(map_manager: MapManager):
 
 func set_width(w: float):
 	width = w
-	for line in highlight3d.node.get_children():
+	for line in highlight3d.viewport.get_children():
 		line.width = w
 
 func set_color(c: Color, should_refresh=true):
 	color = c
-	for line in highlight3d.node.get_children():
+	for line in highlight3d.viewport.get_children():
 		line.default_color = c
 
 func refresh():
@@ -41,11 +41,11 @@ func _refresh_tiles():
 	assert(false)
 
 func _draw_tiles():
-	for tile in highlight3d.node.get_children():
+	for tile in highlight3d.viewport.get_children():
 		tile.queue_free()
 	for tile in tiles:
 		var new_line = _draw_tile(tile)
-		highlight3d.node.add_child(new_line)
+		highlight3d.viewport.add_child(new_line)
 
 func _add_point(line: Line2D, pos: Vector2):
 	line.add_point((pos - half_tile) * 100)
--- highlight3d.gd
+++ highlight3d.gd
@@ -1,11 +1,5 @@
-extends Node3D
+extends Node
 
 class_name Highlight3D
 
-@export var texture: ViewportTexture
 @export var viewport: SubViewport
-@export var sprite: Sprite3D
-@export var node: Node2D
-
-func _ready():
-	sprite.texture = texture
