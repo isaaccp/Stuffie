7a7dbe7f54240059f9b8cf86d22273b717025716
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-02-25 08:44:30 -0500

    Set Sprite3D texture directly to viewport.
    I was using an interediate ViewportTexture that is not necessary.

--- highlight3d.gd
+++ highlight3d.gd
@@ -2,10 +2,5 @@ extends Node3D
 
 class_name Highlight3D
 
-@export var texture: ViewportTexture
+@export var container: Node2D
 @export var viewport: SubViewport
-@export var sprite: Sprite3D
-@export var node: Node2D
-
-func _ready():
-	sprite.texture = texture
--- tiles_highlight.gd
+++ tiles_highlight.gd
@@ -4,7 +4,7 @@ class_name TilesHighlight
 
 func _init(map_manager: MapManager, fixed_tiles: Array):
 	super(map_manager)
-	# Do not clear tiles on refresh.
+
 	clear_on_refresh = false
 	for tile in fixed_tiles:
 		tiles.push_back(tile)
