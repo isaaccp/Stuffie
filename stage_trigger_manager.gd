edd69f0d86c208b5363798f7e2b243a87ea6868c
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-03-04 07:53:02 -0500

    Create stage_trigger_manager.gd

---
+++ stage_trigger_manager.gd
@@ -0,0 +1,41 @@
+extends RefCounted
+
+class_name StageTriggerManager
+
+var triggers: Array[StageTrigger]
+var enemy_deaths = 0
+var spawn_treasure_cb: Callback
+var open_door_cb: Callback
+var close_door_cb: Callback
+
+func _init(triggers: Array[StageTrigger]):
+  self.triggers = triggers
+
+func connect_signals(gameplay: Gameplay):
+  gameplay.enemy_died.connect(on_enemy_died)
+  gameplay.new_turn_started.connect(on_begin_turn)
+  spawn_treasure_cb = gameplay.spawn_treasure
+  # add door callbacks when they exist
+
+func on_enemy_died():
+  enemy_deaths += 1
+  for trigger in triggers:
+    if trigger.trigger_type == StageTrigger.TriggerType.ENEMIES_KILLED:
+      if trigger.enemies_killed == enemy_deaths:
+        call_callback(trigger)
+
+func on_begin_turn(turn_number: int):
+  for trigger in triggers:
+    if trigger.trigger_type == StageTrigger.TriggerType.BEGIN_TURN:
+      if trigger.turn == turn_number:
+        call_callback(trigger)
+
+func call_callback(trigger: StageTrigger):
+  match trigger.effect_type:
+    StageTrigger.EffecfType.SPAWN_CHEST:
+      spawn_treasure_cb.call()
+    StageTrigger.EffectType.OPEN_DOOR:
+      pass
+    StageTrigger.EffectType.CLOSE_DOOR:
+      pass
+    
