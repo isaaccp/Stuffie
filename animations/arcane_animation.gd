f6bee339b5d859570b4a06c9164600c7824f14ad
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-03-03 23:32:15 -0500

    Tweak arcane animation slightly.

--- animations/arcane_animation.gd
+++ animations/arcane_animation.gd
@@ -7,12 +7,13 @@ class_name ArcaneAnimation
 func _ready():
 	global_position = origin
 	particles.scale = Vector3(0.5, 0.5, 0.5)
+	var offset = (target - origin).normalized()
 	tw = create_tween()
-	tw.tween_property(self, "global_position", target, 0.8)
-	tw.tween_property(particles.process_material, "initial_velocity_max", 5, 0.3)
-	tw.tween_property(particles.process_material, "initial_velocity_max", 1, 0.6)
-	#tw.tween_property(particles, "scale", Vector3(2.0, 2.0, 2.0), 0.2)
-	#tw.tween_property(particles, "scale", Vector3(0.0, 0.0, 0.0), 0.3)
+	tw.tween_property(self, "global_position", target - offset * 0.5, 0.8)
+	tw.tween_callback(particles.process_material.set.bind("initial_velocity_max", 20))
+	tw.tween_callback(particles.set.bind("amount", 400))
+	tw.tween_callback(particles.set.bind("lifetime", 0.4))
+	tw.tween_interval(0.3)
 
 func apply_effect_time():
-	return 0.9
+	return 0.85
