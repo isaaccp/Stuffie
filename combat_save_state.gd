b1b80e81383f63ace1dfb25a499bcf359298365b
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-03-11 13:23:23 -0500

    Record treasures are done now.

--- combat_save_state.gd
+++ combat_save_state.gd
@@ -5,4 +5,4 @@ class_name CombatSaveState
 @export var turn_number: int
 @export var enemies: Array[EnemySaveState]
 @export var treasures: Array[TreasureSaveState]
-# TODO: Need to record doors and treasures into state.
+# TODO: Need to record doors into state.
