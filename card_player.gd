d9e23ac8238711cc8ea85fe478504d4973a33d8a
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-03-26 12:53:05 -0400

    Refactor card effects to remove duplication.
    Fixes #94.

--- card_player.gd
+++ card_player.gd
@@ -32,23 +32,27 @@ func get_enemy_map(unit: Unit):
 
 func play_card(unit_card: UnitCard, target_tile: Vector2i, direction: Vector2):
 	await unit_card.apply_self_effects()
+
+	var affected_tiles = unit_card.card.effect_area(direction)
+	# Start effects.
+	if effects_node != null:
+		var effect_time = 0
+		for tile_offset in affected_tiles:
+			var tile = target_tile + tile_offset
+			var effect = animation_manager.get_effect(unit_card.card.target_animation)
+			if effect != null:
+				effect.origin = unit_card.unit.global_position
+				effect.target = map.get_world_position(tile)
+				effects_node.add_child(effect)
+				effect_time = effect.apply_effect_time()
+		if effects_node.get_child_count() != 0:
+			await effects_node.get_tree().create_timer(effect_time, false).timeout
 	if unit_card.card.target_mode == Enum.TargetMode.SELF:
 		# Implement animation for SELF cards.
 		await unit_card.apply_self()
+	# As of now, area effects only work with enemies, should support
+	# friendly area effects too.
 	elif unit_card.card.target_mode in [Enum.TargetMode.ENEMY, Enum.TargetMode.AREA]:
-		var affected_tiles = unit_card.card.effect_area(direction)
-		if effects_node != null:
-			var effect_time = 0
-			for tile_offset in affected_tiles:
-				var tile = target_tile + tile_offset
-				var effect = animation_manager.get_effect(unit_card.card.target_animation)
-				if effect != null:
-					effect.origin = unit_card.unit.global_position
-					effect.target = map.get_world_position(tile)
-					effects_node.add_child(effect)
-					effect_time = effect.apply_effect_time()
-			if effects_node.get_child_count() != 0:
-				await effects_node.get_tree().create_timer(effect_time, false).timeout
 		for tile_offset in affected_tiles:
 			var tile = target_tile + tile_offset
 			var enemy_map = get_enemy_map(unit_card.unit)
@@ -58,19 +62,10 @@ func play_card(unit_card: UnitCard, target_tile: Vector2i, direction: Vector2):
 				if enemy.destroyed:
 					enemy_killed.emit(enemy)
 	elif unit_card.card.target_mode in [Enum.TargetMode.ALLY, Enum.TargetMode.SELF_ALLY]:
-		if effects_node != null:
-			var effect_time = 0
-			var effect = animation_manager.get_effect(unit_card.card.target_animation)
-			if effect != null:
-				effect.origin = unit_card.unit.global_position
-				effect.target = map.get_world_position(target_tile)
-				effects_node.add_child(effect)
-				effect_time = effect.apply_effect_time()
-			if effects_node.get_child_count() != 0:
-				await effects_node.get_tree().create_timer(effect_time, false).timeout
 		var ally_map = get_ally_map(unit_card.unit)
 		var target_unit = ally_map[target_tile]
 		await unit_card.apply_to_ally(target_unit)
+	# Clean up effects.
 	if effects_node != null:
 		for effect in effects_node.get_children():
 			await effect.finished()
