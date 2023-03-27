27afb170417accb94e24b55d64ff6042206efef7
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-03-27 08:48:09 -0400

    Add buff animation and apply it to some cards.
    Could use some more work but better than nothing.

--- animation_manager.gd
+++ animation_manager.gd
@@ -6,12 +6,14 @@ var fire_effect = preload("res://animations/fire_animation.tscn")
 var slash_effect = preload("res://animations/slash_animation.tscn")
 var arcane_effect = preload("res://animations/arcane_animation.tscn")
 var projectile_effect = preload("res://animations/projectile_animation.tscn")
+var buff_effect = preload("res://animations/buff_animation.tscn")
 
 var effects = {
 	Enum.TargetAnimationType.FIRE: fire_effect,
 	Enum.TargetAnimationType.SLASH: slash_effect,
 	Enum.TargetAnimationType.ARCANE: arcane_effect,
 	Enum.TargetAnimationType.PROJECTILE: projectile_effect,
+	Enum.TargetAnimationType.BUFF: buff_effect,
 }
 
 func get_effect(effect: Enum.TargetAnimationType):
---
+++ animations/buff_animation.gd
@@ -0,0 +1,15 @@
+extends TweenAnimation
+
+class_name BuffAnimation
+
+@export var particles: GPUParticles3D
+
+func _ready():
+	global_position = target
+	tw = create_tween()
+	tw.tween_interval(0.5)
+	tw.tween_callback(particles.set.bind("emitting", false))
+	tw.tween_interval(0.3)
+
+func apply_effect_time():
+	return 0.5
