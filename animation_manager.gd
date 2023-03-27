349dd5912d98aa1466b7e2fe4233179da81e33ee
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-03-27 17:57:46 -0400

    Add debuff animation.

--- animation_manager.gd
+++ animation_manager.gd
@@ -7,6 +7,7 @@ var slash_effect = preload("res://animations/slash_animation.tscn")
 var arcane_effect = preload("res://animations/arcane_animation.tscn")
 var projectile_effect = preload("res://animations/projectile_animation.tscn")
 var buff_effect = preload("res://animations/buff_animation.tscn")
+var debuff_effect = preload("res://animations/debuff_animation.tscn")
 
 var effects = {
 	Enum.TargetAnimationType.FIRE: fire_effect,
@@ -14,6 +15,7 @@ var effects = {
 	Enum.TargetAnimationType.ARCANE: arcane_effect,
 	Enum.TargetAnimationType.PROJECTILE: projectile_effect,
 	Enum.TargetAnimationType.BUFF: buff_effect,
+	Enum.TargetAnimationType.DEBUFF: debuff_effect,
 }
 
 func get_effect(effect: Enum.TargetAnimationType):
