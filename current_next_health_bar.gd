910a777d69ff5b207a53520fd215ffa53e6597b0
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-04-08 16:19:06 -0400

    Experiment with blinking health bars, may revert.

--- current_next_health_bar.gd
+++ current_next_health_bar.gd
@@ -2,9 +2,9 @@ extends HBoxContainer
 
 class_name CurrentNextHealthBar
 
-@export var next_hp: ColorRect
-@export var current_hp: ColorRect
-@export var missing_hp: ColorRect
+@export var next_hp: Control
+@export var current_hp: Control
+@export var missing_hp: Control
 
 @export var hide_full = false
 
@@ -12,13 +12,11 @@ var current_health: int
 var max_health: int
 var next_health: int
 
-var current_hp_color: Color
 var is_ready = false
 var tween: Tween
 
 func _ready():
 	is_ready = true
-	current_hp_color = current_hp.color
 	update_health()
 
 func set_health(current: int, max: int, next: int):
@@ -39,12 +37,12 @@ func update_health():
 	next_hp.size_flags_stretch_ratio = next_health
 	current_hp.size_flags_stretch_ratio = (current_health - next_health)
 	missing_hp.size_flags_stretch_ratio = (max_health - current_health)
-	if next_health > 0:
-		if tween:
-			tween.kill()
-	else:
-		if not tween or not tween.is_running():
-			tween = create_tween()
-			tween.set_loops()
-			tween.tween_property(current_hp, "color", Color(1, 1, 1), 0.5)
-			tween.tween_property(current_hp, "color", current_hp_color, 0.5)
+	#if next_health > 0:
+	#	if tween:
+	#		tween.kill()
+	#else:
+	if not tween or not tween.is_running():
+		tween = create_tween()
+		tween.set_loops()
+		tween.tween_property(current_hp, "modulate:a", 0.25, 1.0)
+		tween.tween_property(current_hp, "modulate:a", 1, 1.0)
