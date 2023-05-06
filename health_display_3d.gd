26353d6dd795a99024f3b4e1f896e8810c87a194
Isaac Clerencia <isaac.clerencia@gmail.com>
2023-05-06 09:28:42 -0400

    Futile attempt to get health bars to work on exported project.

--- health_display_3d.gd
+++ health_display_3d.gd
@@ -4,11 +4,13 @@ class_name HealthDisplay3D
 
 @export var sprite: Sprite3D
 @export var health_display: CurrentNextHealthBar
-@export var texture: ViewportTexture
 @export var skull: Control
 
 func _ready():
-	sprite.texture = texture
+	# This causes a lot of error spam if done through exported variables.
+	# I thought those errors were making it not work on the project export,
+	# but even without the errors it doesn't seem to work.
+	sprite.texture = $SubViewport.get_texture()
 
 func set_health(current: int, max: int, next: int):
 	health_display.set_health(current, max, next)
